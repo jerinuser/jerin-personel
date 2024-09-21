6d5483b5ca1015ba1e2af2a2632bb1577851da5c
jerinuser <jerinarockiadass2907@gmail.com>
2024-09-21 12:33:30 +0530

    requirements fille added

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Home route
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return "Welcome to the Home Page!"
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    data = request.form['field_name']
-    # Process data
-    return 'Data submitted!'
+# Hello route with a dynamic user name
+@app.route('/hello/<name>')
+def hello(name):
+    return f"Hello, {name}!"
 
 if __name__ == '__main__':
     app.run(debug=True)
