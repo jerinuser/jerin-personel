77ec5d01e6561df4e4e592bf442736adfac74179
jerinuser <jerinarockiadass2907@gmail.com>
2024-09-21 12:31:18 +0530

    simple flask app created

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-print ("hello world")
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    data = request.form['field_name']
+    # Process data
+    return 'Data submitted!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
